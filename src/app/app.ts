06c9651e3f76e122148fd1f42408c25cf16f88df
miriaamaral <miriaamaralcustodio@gmail.com>
2025-09-22 09:12:13 -0300

    feat: createad page with routes for content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { Home } from "./Pages/home/home";
+import { MenuBar } from "./components/menu-bar/menu-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
 export class App {
   protected readonly title = signal('angular-blog');
